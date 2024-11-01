07ccbbfaf589ead6cb07db4c4b1e316f296498dc
Iman Arjmandnia <aaarjmandnia@gmail.com>
2024-11-01 21:09:07 +0330

    fix: :zap: Fixed the main icon of this website

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,13 +5,16 @@ import ClientLayout from "@/features/client-layout";
 import React from "react";
 
 export const metadata = {
-  title: "Hoorie Masoorian Medical",
-  description: "Website",
+  title: "Hamyar Pesooriazis",
+  description:
+    "This website is for detecting whether you have pesooriazis or not",
 };
 
 function RootLayout({ children }) {
   return (
     <html lang="en" dir="rtl">
+      <link rel="icon" href="/images/logo.png" sizes="any" />
+
       <body
         className={`m-0 box-border flex min-h-screen flex-col p-0 antialiased`}
       >
