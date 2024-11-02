b285d499627046eacd5d4206a8cddebfcdccc898
Iman Arjmandnia <aaarjmandnia@gmail.com>
2024-11-02 03:54:54 +0330

    fix: :art: Fixed the 404 page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -10,12 +10,10 @@ export const metadata = {
 export default function NotFound() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-4 text-center">
-      <h1 className="mb-4 text-4xl font-bold">404 - Page Not Found</h1>
-      <p className="mb-6">
-        Sorry, the page you are looking for does not exist.
-      </p>
+      <h1 className="mb-4 text-4xl font-bold">404 - صفحه یافت نشد</h1>
+      <p className="mb-6">صفحه مورد نظر شما یافت نشد</p>
       <Link href="/" className="text-blue-600 underline">
-        Go back to Home
+        بازگشت به خانه
       </Link>
     </div>
   );
