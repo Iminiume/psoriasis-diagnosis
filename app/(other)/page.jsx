eff43456937753662c3a182e09123a76b0f455c9
Iman Arjmandnia <aaarjmandnia@gmail.com>
2024-11-07 23:13:23 +0330

    feat: :alembic: Added version log in the project

--- app/(other)/page.jsx
+++ app/(other)/page.jsx
@@ -1,7 +1,9 @@
 "use client";
 import Home from "@/view/home";
+import Package from "@/package.json";
 
 function Page() {
+  console.log("Site version: " + Package.version);
   return <Home />;
 }
 
